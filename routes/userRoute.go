c8df6bec7d7916e8e4b4dc183d3339bebc6aef23
Mozaik Ramadhan <142678727+Ozaimoza@users.noreply.github.com>
2024-03-01 21:37:58 +0700

    ipdate

--- routes/userRoute.go
+++ routes/userRoute.go
@@ -13,7 +13,6 @@ func UserRouter(r *gin.Engine) {
 		userGroup.POST("/register", controllers.RegisterUser)
 		userGroup.POST("/login", controllers.LoginUser)
 		userGroup.POST("/logout", middleware.AuthMiddleware, controllers.LogoutUser)
-		// userGroup.GET("/validate", middleware.AuthMiddleware, controllers.Validate)
 		userGroup.PUT("/:userId", middleware.AuthMiddleware, controllers.UpdateUser)
 		userGroup.DELETE("/:userId", middleware.AuthMiddleware, controllers.DeleteUser)
 	}
